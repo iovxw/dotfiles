941313c5042c07b60d3b01327f3a661fe8a886a9
iovxw <iovxw@outlook.com>
2020-04-30 23:37:39 +0800

    Switch to rust-analyzer

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2,5 +2,6 @@
 
 ;; Place your private configuration here
 
+(setq rustic-lsp-server 'rust-analyzer)
 (setq display-line-numbers-type 'relative
       doom-font (font-spec :family "xos4 Terminus" :size 16))
