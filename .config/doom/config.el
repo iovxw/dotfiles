a14db51f37f919c24743113aacd46cc7f75c16f5
iovxw <iovxw@outlook.com>
2021-03-23 23:19:24 +0800

    Doom: Add fallback font, and rescale non-ascii font size

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2,10 +2,13 @@
 
 (setq display-line-numbers-type 'relative
       rustic-lsp-server 'rust-analyzer
-      doom-font (font-spec :family "xos4 Terminus" :size 16))
+      doom-font (font-spec :family "xos4 Terminus" :size 16)
+      doom-unicode-font (font-spec :family "Noto Sans Symbols"))
 
 (defun init-noto-fonts ()
   "NO TOFU!"
   (load! "noto-fonts"))
 
 (add-hook 'doom-init-ui-hook #'init-noto-fonts)
+
+(add-to-list 'face-font-rescale-alist '("Noto Sans*" . 0.68))
